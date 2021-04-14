74c815eb1855d8b3a9030f3a18e9bdfbc82c8f6b
Reimon Ferreira <reimonf@gmail.com>
2021-04-14 20:53:49 -0300

    route user

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+
+  const {name, username} = request.body;
+
+  const user = {
+      id: uuidv4(),
+      name, 
+      username, 
+      todos: []
+    };
+    
+    users.push(user)
+
+    return response.status(201).json(user);rs
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
